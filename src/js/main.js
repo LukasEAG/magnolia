68cf3836be28210ea106bde7bf4e0e343b2ab02e
Lukas EAG <lukas@eagproduction.com>
2024-07-19 19:11:17 +0200

    mobile error

--- src/js/main.js
+++ src/js/main.js
@@ -249,20 +249,20 @@ const mobileMenuItems = document.querySelectorAll('.nav__bar-item')
 
 openMobileMenu.addEventListener('click', () => {
     mobileMenu.classList.add('show')
-    body.style.overflowY = 'hidden'
+    body.style.overflow = 'hidden'
 
 })
 
 closeMobileMenu.addEventListener('click', () => {
     mobileMenu.classList.remove('show')
-    body.style.overflowY = 'visible'
+    body.style.overflow = 'visible'
 
 })
 
 mobileMenuItems.forEach( button => {
     button.addEventListener('click', () => {
         mobileMenu.classList.remove('show')
-        body.style.overflowY = 'visible'
+        body.style.overflow = 'visible'
 
 
     })
